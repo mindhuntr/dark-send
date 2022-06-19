fd142f8220b00b7b20a3e4631d4d454ea83c054d
Irfan Jalal <mindhunter@blinkenshell.org>
2022-06-19 10:44:44 +0530

    Initial commit of dark-send

---
+++ config.py
@@ -0,0 +1,28 @@
+import sys
+import os 
+import configparser 
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+parser = configparser.ConfigParser() 
+fullpath = os.path.expanduser("~/.config/dark-send.conf") 
+
+def generate_conf():
+
+    api_id = input("Enter your api id: ") 
+    api_hash = input("Enter your api hash: ") 
+    string_session = ''
+
+    if api_id and api_hash:
+
+        with TelegramClient(StringSession(),api_id,api_hash) as client: 
+            string_session = client.session.save() 
+
+        parser['dark-send'] = {
+                'api_id': api_id,
+                'api_hash': api_hash, 
+                'string_session': string_session
+                } 
+
+        with open(fullpath, 'w') as f: 
+            parser.write(f) 
---
+++ inquirer.py
@@ -0,0 +1,15 @@
+from __future__ import print_function, unicode_literals
+
+from PyInquirer import style_from_dict, Token, prompt, Separator
+from pprint import pprint
+
+
+style = style_from_dict({
+    Token.Separator: '#cc5454',
+    Token.QuestionMark: '#673ab7 bold',
+    Token.Selected: '#cc5454',  # default
+    Token.Pointer: '#673ab7 bold',
+    Token.Instruction: '',  # default
+    Token.Answer: '#f44336 bold',
+    Token.Question: '',
+})
