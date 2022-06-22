99861c9d656f48c6397621642b0fc747d6e4c3c1
Irfan Jalal <mindhunter@blinkenshell.org>
2022-06-22 09:42:06 +0530

    modified rgb color codes

--- inquirer.py
+++ inquirer.py
@@ -5,9 +5,9 @@ from pprint import pprint
 
 
 style = style_from_dict({
-    Token.Separator: '#cc5454',
+    Token.Separator: '#96cdfb',
     Token.QuestionMark: '#673ab7 bold',
-    Token.Selected: '#cc5454',  # default
+    Token.Selected: '#96cdfb',  # default
     Token.Pointer: '#673ab7 bold',
     Token.Instruction: '',  # default
     Token.Answer: '#f44336 bold',
