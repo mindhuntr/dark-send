8b2227d6779a8873ef471ebece9c690c6da25127
Irfan Jalal <mindhunter@blinkenshell.org>
2022-06-27 10:17:51 +0530

    added progress_bar.py

---
+++ progress_bar.py
@@ -0,0 +1,82 @@
+from alive_progress import alive_bar 
+from time import sleep 
+from threading import Thread
+
+flag,no_iter,prev_count = False, 0, 0
+num_arr = []
+no_thread = 0
+
+def percentage(part,whole): 
+    per_num = 100 * float(part)/float(whole)
+    return round(per_num,0)
+
+def progress(current,total):
+      # print('Uploaded', current, 'out of', total,
+      #      'bytes: {:.2%}'.format(current / total))
+
+      global num_arr
+      global flag
+      global prev_count
+      global no_thread
+
+      if no_thread == 0: 
+          bar_thread = Thread(target=bar_func)
+          bar_thread.start() 
+          no_thread = 1
+
+      bar_count = percentage(current,total)
+
+      if not bar_count in num_arr:
+          if not num_arr:
+              prev_count = bar_count
+              for i in range(int(prev_count)):
+                  while True:
+                      if flag == False:
+                          flag = True
+                          break
+
+              num_arr.append(bar_count)
+              if bar_count == 100:
+                  for i in range(int(prev_count),100): 
+                      while True:
+                          flag = True
+
+                  prev_count,no_thread = 0, 0 
+                  num_arr = []
+                  return
+          else:
+              for i in range(int(bar_count - prev_count)):
+                  while True:
+                      if flag == False: 
+                          flag = True
+                          break
+
+              prev_count = bar_count
+              num_arr.append(bar_count)
+
+              if bar_count == 100:
+                  for i in range(int(prev_count),100): 
+                      while True:
+                          flag = True
+
+                  prev_count,no_thread = 0, 0 
+                  num_arr = []
+                  return
+
+
+def bar_func(): 
+   
+    global no_iter
+    global flag
+
+    with alive_bar(100) as bar: 
+        while True: 
+            if no_iter == 100: 
+                no_iter = 0 
+                break 
+
+            if flag == True: 
+                bar() 
+                no_iter += 1
+                flag = False 
+
