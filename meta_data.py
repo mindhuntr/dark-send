01a5436381de0c0cd64ec1f1d2a02464be8fc8ff
Irfan Jalal <mindhunter@blinkenshell.org>
2022-07-28 09:34:03 +0530

    fixed error when extracting metadata for .webm

--- meta_data.py
+++ meta_data.py
@@ -1,7 +1,6 @@
 from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
 
-
 def meta_extract(filename): 
 
     parser = createParser(filename) 
@@ -10,16 +9,22 @@ def meta_extract(filename):
         print("Unable to parse file") 
         return 1 
 
-    try: 
+    try:
         metadata = extractMetadata(parser) 
+        meta_dict = vars(metadata) 
 
-        duration = metadata.get('duration').seconds
-        width = metadata.get('width')
-        height = metadata.get('height') 
+        if '_MultipleMetadata__key_counter' not in meta_dict: 
+            duration = metadata.get('duration').seconds
+            width = metadata.get('width') 
+            height = metadata.get('height')
+        else:
+            duration = metadata.get('duration').seconds
+            width = meta_dict['_MultipleMetadata__groups']['video[1]'].get('width')
+            height = meta_dict['_MultipleMetadata__groups']['video[1]'].get('height')
+        
+        return height, width, duration 
 
-        return height,width,duration 
-    
     except: 
-        print("Unable to extract metadata") 
-        return 1 
 
+        print("Unable to extract metadata") 
+        return 1, 1, 1
