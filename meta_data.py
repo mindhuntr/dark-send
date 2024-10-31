cd973a29b971c575cb508a8c2657fa54b324002c
Irfan Jalal <mindhunter@blinkenshell.org>
2024-10-31 17:20:46 +0530

    optimized speed and removed thumbnail snippet

--- meta_data.py
+++ meta_data.py
@@ -1,8 +1,5 @@
 from hachoir.parser import createParser
 from hachoir.metadata import extractMetadata
-from moviepy.editor import VideoFileClip
-from PIL import Image
-import io
 
 def meta_extract(filename): 
 
@@ -31,31 +28,4 @@ def meta_extract(filename):
 
         print("Unable to extract metadata") 
         return 1, 1, 1
-
-def extract_thumb(thumbnail, video_path, time=1, size=(320, 320)): 
-
-    if thumbnail: 
-        with Image.open(thumbnail) as img: 
-
-            img_resized = img.resize(size)
-            img_byte_arr = io.BytesIO()
-
-            img_resized.save(img_byte_arr, format='JPEG')
-
-            img_byte_arr.seek(0)  # Go to the start of the BytesIO object
-            img_bytes = img_byte_arr.read()
-            
-            return img_bytes
-
-    clip = VideoFileClip(video_path)
-    frame = clip.get_frame(time)
-    frame_image = Image.fromarray(frame)
-
-    frame_image = frame_image.resize(size)
-    
-    byte_io = io.BytesIO()
-    frame_image.save(byte_io, format='JPEG')
-    byte_io.seek(0)
-
-    return byte_io
 
